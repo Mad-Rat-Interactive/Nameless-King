68bb321179c1972414c3ab3b786d9301c668224d
thr4sha <afbos72@gmail.com>
2024-09-01 23:50:48 +1200

    Updated movement
    Tied movement to physics engine instead of frame rate

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @export var speed = 200
 
-# Called every frame.
-func _process(delta):
+# Movement synchronized with physics steps, independant of framerate
+func _physics_process(delta):
 	velocity = Vector2.ZERO # Player movement vector.
 	
 	if Input.is_action_pressed("up"):
