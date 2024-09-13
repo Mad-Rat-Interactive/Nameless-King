5f1a9cc61ab7ce837f58270adb48ab0b3031d1f5
thr4sha <afbos72@gmail.com>
2024-09-13 20:47:58 +1200

    Fixed errors + made big map
    Fixed 2 debug coding errors and made a HUGE map.

--- scripts/player.gd
+++ scripts/player.gd
@@ -40,13 +40,13 @@ func _physics_process(delta):
 	update_animation_parameter(velocity)
 
 # Called every frame, handles other updates
-func _process(delta):
+func _process(_delta):
 	pass
 
-# Update animation parameters based on the player's velocity
-func update_animation_parameter(velocity: Vector2):
+# Update animation parameters based on the player's movement velocity
+func update_animation_parameter(movement: Vector2):
 	# Set the conditions in the AnimationTree
-	if velocity == Vector2.ZERO:
+	if movement == Vector2.ZERO:
 		animation_tree["parameters/conditions/Idle"] = true
 		animation_tree["parameters/conditions/is_moving"] = false
 		
@@ -65,9 +65,9 @@ func update_animation_parameter(velocity: Vector2):
 	else:
 		animation_tree["parameters/Idle/blend_position"] = Vector2(-1, 0)
 		
-	# Update the blend position based on the velocity
-	if velocity != Vector2.ZERO:
-		animation_tree["parameters/Walk/blend_position"] = velocity.normalized()
+	# Update the blend position based on the movement velocity
+	if movement != Vector2.ZERO:
+		animation_tree["parameters/Walk/blend_position"] = movement.normalized()
 	
 
 # Called when the idle timer reaches zero
