db6f5821e58e1c0f2c925be5b9af739b8e9097cf
thr4sha <afbos72@gmail.com>
2024-09-16 12:06:57 +1200

    Pillar system done

--- autoloads/scenes/Pillars.gd
+++ autoloads/scenes/Pillars.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var pillar1_inZone = false
 var pillar2_inZone = false
+var pillar3_inZone = false
+var pillar4_inZone = false
 
 func _physics_process(delta):
 	activate_pillar()
@@ -15,6 +17,14 @@ func activate_pillar():
 		if !(Global.pillar_2):
 			if pillar2_inZone:
 				Global.pillar_2 = true
+				print("Pillar activated")
+		if !(Global.pillar_3):
+			if pillar3_inZone:
+				Global.pillar_3 = true
+				print("Pillar activated")
+		if !(Global.pillar_4):
+			if pillar4_inZone:
+				Global.pillar_4 = true
 				print("Pillar activated")
 
 func _on_pillar_1_body_entered(body):
@@ -35,3 +45,19 @@ func _on_pillar_2_body_exited(body):
 		pillar2_inZone = false
 
 
+func _on_pillar_3_body_entered(body):
+	if body.has_method("player"):
+		pillar3_inZone = true
+
+func _on_pillar_3_body_exited(body):
+	if body.has_method("player"):
+		pillar3_inZone = false
+
+
+func _on_pillar_4_body_entered(body):
+	if body.has_method("player"):
+		pillar4_inZone = true
+
+func _on_pillar_4_body_exited(body):
+	if body.has_method("player"):
+		pillar4_inZone = false
--- autoloads/singletons/global.gd
+++ autoloads/singletons/global.gd
@@ -5,3 +5,5 @@ var player_current_attack = false
 #Pillars active
 var pillar_1 = false
 var pillar_2 = false
+var pillar_3 = false
+var pillar_4 = false
