f88ab063578195bc7670fa06dd05c17c49d359a0
BlackPri0r <lawrenceli630@gmail.com>
2024-09-16 15:59:56 +1200

    stuff

---
+++ autoloads/scenes/ending.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func _ready():
+	SoundManager.play_sound("Sad")
--- autoloads/singletons/global.gd
+++ autoloads/singletons/global.gd
@@ -7,3 +7,8 @@ var pillar_1 = false
 var pillar_2 = false
 var pillar_3 = false
 var pillar_4 = false
+
+func _physics_process(delta):
+	if pillar_1 and pillar_2 and pillar_3 and pillar_4:
+		SoundManager.play_sound("Sad")
+		get_tree().change_scene_to_file("res://autoloads/scenes/ending.tscn")
