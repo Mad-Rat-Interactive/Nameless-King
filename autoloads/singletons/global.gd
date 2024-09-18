01688dc3feb810eae4fdcab8d62c4d0f691e6d15
thr4sha <afbos72@gmail.com>
2024-09-18 22:40:19 +1200

    Added pillar light effect on activation

--- autoloads/singletons/global.gd
+++ autoloads/singletons/global.gd
@@ -8,6 +8,6 @@ var pillar_2 = false
 var pillar_3 = false
 var pillar_4 = false
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if pillar_1 and pillar_2 and pillar_3 and pillar_4:
 		get_tree().change_scene_to_file("res://autoloads/scenes/cutscene.tscn")
---
+++ scripts/pillarlight.gd
@@ -0,0 +1,19 @@
+extends TileMap
+
+#Loading child light nodes
+@onready var p1 = $Pillar1_light
+@onready var p2 = $Pillar2_light
+@onready var p3 = $Pillar3_light
+@onready var p4 = $Pillar4_light
+
+#Upon input checks if pillar has been activated and turns on light if it has
+func _process(_delta):
+	if Input.is_action_just_pressed("interact"):
+		if Global.pillar_1:
+			p1.show()
+		if Global.pillar_2:
+			p2.show()
+		if Global.pillar_3:
+			p3.show()
+		if Global.pillar_4:
+			p4.show()
